3d50c0eedb0449df32cff211a4898aca925dc2d6
Johnnyauo <48042152+BT081056@users.noreply.github.com>
2020-10-27 15:34:34 +0800

    Create ui.py

---
+++ ui.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'MainWindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1200, 640)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.img_output = QtWidgets.QLabel(self.centralwidget)
+        self.img_output.setGeometry(QtCore.QRect(10, 0, 575, 432))
+        self.img_output.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.img_output.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.img_output.setObjectName("img_output")
+        self.img_output_2 = QtWidgets.QLabel(self.centralwidget)
+        self.img_output_2.setGeometry(QtCore.QRect(610, 0, 576, 432))
+        self.img_output_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.img_output_2.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.img_output_2.setObjectName("img_output_2")
+        self.start_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.start_btn.setGeometry(QtCore.QRect(470, 480, 200, 100))
+        font = QtGui.QFont()
+        font.setFamily("微軟正黑體")
+        font.setPointSize(18)
+        font.setBold(False)
+        font.setWeight(50)
+        self.start_btn.setFont(font)
+        self.start_btn.setObjectName("start_btn")
+        self.get_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.get_btn.setGeometry(QtCore.QRect(10, 480, 200, 100))
+        font = QtGui.QFont()
+        font.setFamily("微軟正黑體")
+        font.setPointSize(18)
+        font.setBold(False)
+        font.setWeight(50)
+        self.get_btn.setFont(font)
+        self.get_btn.setObjectName("get_btn")
+        self.VALUE = QtWidgets.QLabel(self.centralwidget)
+        self.VALUE.setGeometry(QtCore.QRect(700, 490, 261, 80))
+        font = QtGui.QFont()
+        font.setFamily("Gill Sans MT")
+        font.setPointSize(25)
+        self.VALUE.setFont(font)
+        self.VALUE.setObjectName("VALUE")
+        self.align_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.align_btn.setGeometry(QtCore.QRect(240, 480, 200, 100))
+        font = QtGui.QFont()
+        font.setFamily("微軟正黑體")
+        font.setPointSize(18)
+        font.setBold(False)
+        font.setWeight(50)
+        self.align_btn.setFont(font)
+        self.align_btn.setObjectName("align_btn")
+        self.close_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.close_btn.setGeometry(QtCore.QRect(980, 480, 200, 100))
+        font = QtGui.QFont()
+        font.setFamily("微軟正黑體")
+        font.setPointSize(18)
+        font.setBold(False)
+        font.setWeight(50)
+        self.close_btn.setFont(font)
+        self.close_btn.setObjectName("close_btn")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1200, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.img_output.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\">Output Image</p></body></html>"))
+        self.img_output_2.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\">Output Image</p></body></html>"))
+        self.start_btn.setText(_translate("MainWindow", "量測"))
+        self.get_btn.setText(_translate("MainWindow", "圖片截取"))
+        self.VALUE.setText(_translate("MainWindow", "LABEL"))
+        self.align_btn.setText(_translate("MainWindow", "A4調校"))
+        self.close_btn.setText(_translate("MainWindow", "休息"))
+
